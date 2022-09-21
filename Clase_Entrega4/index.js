a18bfbdf6b60781f2ac1b814cc8a1606022e45b7
Lucas18991 <lucas_18991@hotmail.com>
2022-09-21 10:08:56 -0300

    Finalizacion ejercitacion

---
+++ Clase_Entrega4/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const productRoutes = require('./routes/index.js')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+'/public'))
+app.use('/api/productos', productRoutes)
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/BACKEND/Clase_Entrega4/files/index.html') 
+})
+const PORT = process.env.port || 8080
+app.listen(PORT,() =>{
+    console.log(`Escuchando el puerto ${PORT}`)
+})
+.on('error',(error) => console.log(error))
